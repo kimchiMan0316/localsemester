3359bd7224f6f39bc1271b9a63f6e598f82091b6
parkseolho <taeyoung6045795@gmail.com>
2025-06-18 15:19:47 +0900

    WIP: 임시 커밋

--- src/page/post/postDetail.jsx
+++ src/page/post/postDetail.jsx
@@ -3,67 +3,70 @@ import { useParams, useNavigate } from "react-router-dom";
 import { PostViewer } from "../../components/form/postForm/postViewer";
 import { Container } from "../../components/container/container";
 import { useMyProfile } from "../../store/myprofile";
-import { CommentBox } from "../..//components/box/commentBox"
+import { CommentBox } from "../..//components/box/commentBox";
 import { CommentForm } from "../../components/form/commentForm/commentForm";
 
 export const PostDetail = () => {
-    const { id } = useParams();
-    const { id: myId } = useMyProfile((state) => state.myProfile);
-    const [posts, setPosts] = useState(null);
-    const [comments, setComments] = useState([]);
-    const me = posts?.userId === myId;
-    // 게시글 작성자와 현재 로그인한 사용자가 같은지 확인
-    const navigator = useNavigate();
+  const { id } = useParams();
+  const { id: myId } = useMyProfile((state) => state.myProfile);
+  const [posts, setPosts] = useState(null);
+  const [comments, setComments] = useState([]);
+  const me = posts?.userId === myId;
+  // 게시글 작성자와 현재 로그인한 사용자가 같은지 확인
+  const navigator = useNavigate();
 
-    useEffect(() => {
-        const getPost = async () => {
-            try {
-                const req = await fetch(`/post/${id}`);
-                const res = await req.json();
-                setPosts(res);
-
-            } catch (error) {
-                console.error("게시글 가져오기 중 오류 발생:", error);
-            }
-        }
-
-        // 게시글 정보 가져오기
-        getPost();
-
-
-
-
-    }, [id]);
-
-    useEffect(() => {
-        // 댓글 정보 가져오기
-        fetch(`/postComment`)
-            .then((res) => res.json())
-            .then((data) => {
-                setComments(data);
-            })
-            .catch((error) => {
-                console.error("댓글 가져오기 중 오류 발생:", error);
-            });
-    }, []);
-
-    const getcomments = (newComments) => {
-        setComments((state) => [...state, newComments]);
-        console.log(comments);
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+        const req = await fetch(`/post/${id}`);
+        const res = await req.json();
+        setPosts(res);
+      } catch (error) {
+        console.error("게시글 가져오기 중 오류 발생:", error);
+      }
     };
 
-    if (!posts) return <p>로딩 중...</p>;
+    // 게시글 정보 가져오기
+    getPost();
+  }, [id]);
 
-    return (
-        <>
-            <Container className="relative pb-64">
-                <PostViewer item={posts} me={me} url="/post" deletePosting={() => navigator(-1)} />
-            </Container>
-            <Container>
-                <CommentForm articleId={id} url={"/postComment"} getComment={getcomments} />
-                <CommentBox comment={comments} url={"/postComment"} />
+  useEffect(() => {
+    // 댓글 정보 가져오기
+    fetch(`/postComment?postId=${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setComments(data);
+      })
+      .catch((error) => {
+        console.error("댓글 가져오기 중 오류 발생:", error);
+      });
+  }, []);
 
-            </Container>
-        </>
-    );
+  const getcomments = (newComments) => {
+    setComments((state) => [...state, newComments]);
+    console.log(comments);
+  };
+
+  if (!posts) return <p>로딩 중...</p>;
+
+  return (
+    <>
+      <Container className="relative pb-64">
+        <PostViewer
+          item={posts}
+          me={me}
+          url="/post"
+          deletePosting={() => navigator(-1)}
+        />
+      </Container>
+      <Container>
+        <CommentForm
+          articleId={id}
+          url={"/postComment"}
+          getComment={getcomments}
+        />
+        <CommentBox comment={comments} url={"/postComment"} />
+      </Container>
+    </>
+  );
 };
